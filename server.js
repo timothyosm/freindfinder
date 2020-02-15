36bcb28cd4295eac765732749301e1dcf46b0a3e
Timothy Osmond <Timothy.Osm@gmail.com>
2020-02-15 22:48:44 +0800

    fixed server logic

--- server.js
+++ server.js
@@ -10,9 +10,14 @@ var PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
 require('./app/routing/apiRoutes')(app);
 require('./app/routing/htmlRoutes')(app, path);
 
 app.listen(PORT, ()=> {
     console.log(`Connected on port: ${PORT}`);
 });
+
